3e1d83a412c29206e3e272376eafec6d7bb47ce8
tikzoxs <tikzoxs@gmail.com>
2020-06-20 01:13:51 +1200

    saving model weights and loading works fine

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 python train.py \
-  --train_batch_size 1 \
-  --workdir "/home/tharindu/Desktop/black/codes/Black/CV_SSL/Resnet50v2/checkpoint"\
+  --train_batch_size 8 \
+  --workdir "/home/tharindu/Desktop/black/codes/Black/EyeKnowYou_SSL/ckpt"\
   --train_folder "/media/tharindu/Transcend/Tharindu/EyeKnowYouSSLData/frames"\
   --validation_folder "/media/tharindu/Transcend/Tharindu/EyeKnowYouSSLData/frames"\
   --test_folder "/media/tharindu/Transcend/Tharindu/EyeKnowYouSSLData/frames"\
+  --tensorboard_logs_directory "./logs"\
   --epochs 35 \
+  --steps_per_epoch 100
   "$@"
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from tensorflow import keras
 from keras.callbacks import ModelCheckpoint
 
 import tensorflow as tf
+import os
 
 import absl.app as app
 import absl.flags as flags
@@ -26,6 +27,9 @@ flags.DEFINE_string('test_folder', None, 'Where are the testing images. Should b
 flags.DEFINE_string('train_batch_size', None, 'train_batch_size')
 flags.mark_flag_as_required('train_batch_size')
 
+flags.DEFINE_string('steps_per_epoch', None, 'steps_per_epoch')
+flags.mark_flag_as_required('steps_per_epoch')
+
 flags.DEFINE_string('validation_batch_size', None, 'validation_batch_size')
 # flags.mark_flag_as_required('validation_batch_size')
 
@@ -35,27 +39,34 @@ flags.DEFINE_string('test_batch_size', None, 'test_batch_size')
 flags.DEFINE_string('epochs', None, 'epochs')
 # flags.mark_flag_as_required('epochs')
 
+flags.DEFINE_string('tensorboard_logs_directory', None, 'TB dir')
+flags.mark_flag_as_required('tensorboard_logs_directory')
 
 
 def main(unused_argv):
 	# logging.info('config: %s', FLAGS)
 	logging.info('workdir: %s', FLAGS.workdir)
+	ckpt = FLAGS.get_flag_value('workdir', None) + "/weights.h5"
 
-	eyeknowyou_ssl_model = keras.applications.resnet_v2.ResNet50V2(include_top=False, weights=None, input_tensor=None, input_shape=(128,128,1), pooling='avg', classes=4)
+	if(os.path.isfile(ckpt)):
+		eyeknowyou_ssl_model = keras.applications.resnet_v2.ResNet50V2(include_top=False, weights=ckpt, input_tensor=None, input_shape=(128,128,1), pooling='avg', classes=4)
+	else:
+		eyeknowyou_ssl_model = keras.applications.resnet_v2.ResNet50V2(include_top=False, weights=None, input_tensor=None, input_shape=(128,128,1), pooling='avg', classes=4)
+
 	eyeknowyou_ssl_model.summary()
 	eyeknowyou_ssl_model.compile(optimizer=keras.optimizers.Adadelta(), 
 	              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
 	              metrics=['sparse_categorical_accuracy'])
-	checkpointer = ModelCheckpoint(filepath=FLAGS.get_flag_value('workdir', None), verbose=1, save_best_only=True)
+	checkpointer = ModelCheckpoint(filepath=ckpt, verbose=1, save_best_only=True)
 	reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
 	                              patience=10, min_lr=0.001)
-	tensorboard = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, 
+	tensorboard = keras.callbacks.TensorBoard(log_dir=FLAGS.get_flag_value('tensorboard_logs_directory', './logs'), histogram_freq=0, 
 		batch_size=int(FLAGS.get_flag_value('train_batch_size', None)), write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, 
 		embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
 	history = eyeknowyou_ssl_model.fit_generator(
 		generators.eyeknowyouTrainGenerator(), 
-		steps_per_epoch=None, epochs=int(FLAGS.get_flag_value('epochs', None)), verbose=1, callbacks=[checkpointer,tensorboard,reduce_lr], 
-		validation_data=None, validation_steps=None, validation_freq=1, class_weight=None, 
+		steps_per_epoch=int(FLAGS.get_flag_value('steps_per_epoch', None)), epochs=int(FLAGS.get_flag_value('epochs', None)), verbose=1, callbacks=[checkpointer,tensorboard,reduce_lr], 
+		validation_data=generators.eyeknowyouTrainGenerator(), validation_steps=50, validation_freq=1, class_weight=None, 
 		max_queue_size=10, workers=1, use_multiprocessing=False, shuffle=True, initial_epoch=0)
 
 	print('\nhistory dict:', history.history)
